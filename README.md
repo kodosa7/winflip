a8341864229745443f3fb71d5a72982a021d30ff
kodosa7 <iam.treeleafgirl@gmail.com>
2022-02-01 14:38:27 +0100

    new version, gets window size automatically, modified layout, still errors

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
 # WinFlip
 A Win32 tool mirroring the screen content
-e.g. for video calls showing the same screen as broadcasted.
-Useful for apps like MS Teams or Skype with no screen mirror option.
+e.g. for video calls showing the same screen as broadcasted, suitable
+for reading texts. Useful for apps like MS Teams or Skype with no screen mirror option.
 
 ## requirements
 - Python 3
 - see requirements.txt
 or
 - just a Win32 environment when running binary
-- minimum resolution 1920x1080, Windows default scale 100%, two displays recommended (1st for source screen, 2nd for WinFlip output)
+- Windows default scale 100%, two displays recommended (1st for source screen, other for WinFlip output)
 - powerful CPU :)
 
 ## run
 ```python winflip.py```
 or
 ```winflip.exe```
-- set grab area (top left and bottom right pixel on the source screen)
+- open Skype or MS Teams video call window (that one with camera view)
+- launch WinFlip
 - set framerate (5 is default, less than 5 could cause system slowdown)
-- press Go and move the output window to a desired location
-- press [X] to exit the program
+- choose Go Skype or Go MS Teams
+- move the output window to a desired location (not the source screen if possible)
+- when resizing source window, the WinFlip window is autmatically resized as well
+- press [X] to exit WinFlip
--- winflip.py
+++ winflip.py
@@ -2,9 +2,12 @@
 # (C) 2022 ELS
 
 import tkinter, os, sys
+import win32gui
 from tkinter import *
 from PIL import Image, ImageTk, ImageGrab
 
+
+SEARCH_TEXT = ["Schůzka"]  # set to Teams at first
 
 # get path (for python 3.8+) to be able to compile as one file using PyInstaller
 def resource_path(relative_path):
@@ -21,42 +24,86 @@ tk = Tk()
 icon_filename = resource_path("winflip.ico")
 tk.iconbitmap(icon_filename)
 
-wintitle = "WinFlip 0.5 © 2022 ELS"
+wintitle = "WinFlip 0.8"
 label = tkinter.Label(tk)
 label.pack()
 tkimg = [None]
-X0, Y0, X1, Y1 = 0, 0, 1920, 1080  # initial grab area
+# X0, Y0, X1, Y1 = 0, 0, 1920, 1080  # initial grab area
 
 tk.geometry("400x200")  # set initial window size
 message = Label(tk, text="Set parameters, then press Go", font=("Calibri", 12))
 author_name = Label(tk, text="© 2022 ELS")
-upper_left_label = Label(tk, text="Upper Left")
-bottom_right_label = Label(tk, text="Bottom right")
 framerate_label = Label(tk, text="Framerate")
 
+def set_skype():
+    global SEARCH_TEXT
+    SEARCH_TEXT = ["Skype"]
 
-def get_slider_values():
-    return slider_upper_x.get(), slider_upper_y.get(), slider_bottom_x.get(), slider_bottom_y.get(), slider_framerate.get()
+def set_teams():
+    global SEARCH_TEXT
+    SEARCH_TEXT = ["Schůzka"]
+
+def my_callback(hwnd, extra):
+    """ callback for EnumWindows function
+        to get spicific window coords.
+        Can't return values so global var is set."""
+
+    rect = win32gui.GetWindowRect(hwnd)
+    x = rect[0]
+    y = rect[1]
+    w = rect[2] - x
+    h = rect[3] - y
+
+    global new_rect
+    if SEARCH_TEXT[0] in win32gui.GetWindowText(hwnd):
+        new_rect = [x, y, w, h]
+
+def get_all_window_sizes():
+    """ Function to get coords from a window name
+        specified in my_callback() """
+
+    win32gui.EnumWindows(my_callback, None)
+    return new_rect
+
+def get_slider_framerate():
+    """ Gets value from slider """
+    return slider_framerate.get()
 
 def hide_elements():
     """ hide all window elements """
+    skype_button.place_forget()
+    teams_button.place_forget()
     go_button.place_forget()
-    slider_upper_x.place_forget()
-    slider_upper_y.place_forget()
-    slider_bottom_x.place_forget()
-    slider_bottom_y.place_forget()
     slider_framerate.place_forget()
     message.place_forget()
     author_name.place_forget()
-    upper_left_label.place_forget()
-    bottom_right_label.place_forget()
     framerate_label.place_forget()
 
 def loopcapture():
-    """ main loop """
+    """ Main loop
+        - hides window elements
+        - gets a framerate from slider
+        - counts coords not to be <10
+        - sets a window size from coords
+        - grabs the window size to an image
+        - flips it over """
+
+    print(SEARCH_TEXT)
     hide_elements()
-    nx0, ny0, nx1, ny1, fr = get_slider_values()  # get new slider values
-    tk.geometry(str(nx1 - nx0) + "x" + str(ny1 - ny0))  # size it
+    fr = get_slider_framerate()  # get slider fr value
+    
+    nx0, ny0, nx1, ny1 = get_all_window_sizes()
+
+    rx = nx1 - nx0
+    ry = ny1 - ny0
+    if rx <= 10:
+        rx = 10
+    if ry <= 10:
+        ry = 10
+
+    # print("real xy: ", rx, "x", ry)
+
+    tk.geometry(str(rx) + "x" + str(ry))  # size it
 
     flip_method = Image.FLIP_LEFT_RIGHT
     area = (nx0, ny0, nx1, ny1)
@@ -72,31 +119,19 @@ def loopcapture():
 # show texts
 message.place(x=10, y=14)
 author_name.place(x=330, y=180)
-upper_left_label.place(x=42, y=50)
-bottom_right_label.place(x=136, y=50)
 framerate_label.place(x=220, y=50)
 
-# configure sliders and button
-slider_upper_x = Scale(tk, from_=0, to=1920)
-slider_upper_x.set(0)
-slider_upper_x.place(x=10, y=70)
-slider_upper_y = Scale(tk, from_=0, to=1080)
-slider_upper_y.set(0)
-slider_upper_y.place(x=60, y=70)
-
-slider_bottom_x = Scale(tk, from_=320, to=1920)
-slider_bottom_x.set(1920)
-slider_bottom_x.place(x=110, y=70)
-slider_bottom_y = Scale(tk, from_=200, to=1080)
-slider_bottom_y.set(1080)
-slider_bottom_y.place(x=160, y=70)
-
+# configure slider and button
 slider_framerate = Scale(tk, from_=1, to=100)
 slider_framerate.set(5)
 slider_framerate.place(x=220, y=70)
 
+skype_button = tkinter.Button(tk, text="Skype", command=set_skype)
+skype_button.place(x=10, y=130, height=50, width=80)
+teams_button = tkinter.Button(tk, text="MS Teams", command=set_teams)
+teams_button.place(x=100, y=130, height=50, width=80)
 go_button = tkinter.Button(tk, text="Go!", command=loopcapture)  # launch capture window
-go_button.place(x=290, y=130, height=50, width=80)
+go_button.place(x=310, y=130, height=50, width=80)
 
 
 if __name__ == "__main__":
